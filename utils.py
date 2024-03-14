84741b61cfded08cb4709a4f7c2949c0a9c75732
Zdenek Rovnez <80111494+ZdenekRovnez@users.noreply.github.com>
2024-03-14 15:29:38 +0100

    added utility functions for time series

--- utils.py
+++ utils.py
@@ -1,3 +1,37 @@
-""""
-Placeholder code for pyzden
-"""
+import datetime
+
+
+def inverse_dict(dictionary):
+    return {v: k for k, v in dictionary.items()}
+
+
+def can_convert_to_int(value) -> bool:
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
+
+
+def can_convert_to_datetime(year: int, week: int) -> bool:
+    try:
+        datetime.date.fromisocalendar(year, week, 1)
+        return True
+    except ValueError:
+        return False
+
+
+def floor_subset(number, subset):
+    subset = sorted(subset)
+    for value in reversed(subset):
+        if number >= value:
+            return value
+    return subset[0] if subset else None
+
+
+def ceil_subset(number, subset):
+    subset = sorted(subset)
+    for value in subset:
+        if number <= value:
+            return value
+    return subset[-1] if subset else None
