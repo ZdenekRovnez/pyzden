af3ed0aaedaa1c6762c019bd06ffb71268f3348e
Zdenek Rovnez <80111494+ZdenekRovnez@users.noreply.github.com>
2024-03-14 12:30:22 +0100

    minor changes for test

--- test_workflow.py
+++ test_workflow.py
@@ -4,7 +4,7 @@ import math
 def some_function():
     first_number = 1
     second_number = 2
-    # This is a problematic computation for demonstration
+    # This is a problematic computation for demonstration!
     result = math.pow(first_number, second_number)
     print("The result is:", result)
 
