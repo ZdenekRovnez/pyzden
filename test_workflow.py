adb0036d74ac215746040c80a65c98ad73143eb0
Zdenek Rovnez <80111494+ZdenekRovnez@users.noreply.github.com>
2024-03-14 11:40:08 +0100

    Create test_workflow.py
    dummy file for testing the workflow

---
+++ test_workflow.py
@@ -0,0 +1,9 @@
+import math, sys;
+
+def some_function():
+ x = 1
+ y=2
+ # This is a problematic computation for demonstration
+ result=math.pow(x,y)
+ print("The result is:",result)
+if __name__=="__main__":some_function()
